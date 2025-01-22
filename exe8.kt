077ad5cd7daee755ce73600af167a5bf54c7a605
Ceejprogram <c.licayan.140752.tc@umindanao.edu.ph>
2025-01-22 19:03:24 +0800

    #8 Greatest Number Indentifier

--- exe8.kt
+++ exe8.kt
@@ -4,23 +4,19 @@ fun main() {
 
     // Enter 3 numbers
     print("Enter first number: ")
-    var num1 = readLine()?.toIntOrNull()
+    val num1 = readLine()?.toIntOrNull()
     print("Enter second number: ")
-    var num2 = readLine()?.toIntOrNull()
+    val num2 = readLine()?.toIntOrNull()
     print("Enter third number: ")
-    var num3 = readLine()?.toIntOrNull()
+    val num3 = readLine()?.toIntOrNull()
 
     // Check if there is an empty input
     if (num1 != null && num2 != null && num3 != null) {
         // Determine the greatest number
-        val greatest = when {
-            num1 >= num2 && num1 >= num3 -> num1
-            num2 >= num1 && num2 >= num3 -> num2
-            else -> num3
-        }
+        val greatest = maxOf(num1, num2, num3)
         // Print the greatest number
         println("The greatest number is: $greatest")
     } else {
-        println("Error: Invalid Input")
+        println("Error: Please enter a number!")
     }
 }
